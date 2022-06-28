a02b30780797f98cb815242adfa55a43b3d07644
polechka <pol1na-tsukanova@yandex.ru>
2022-06-28 16:41:05 +0300

    fix: script functions, profile

--- script/script.js
+++ script/script.js
@@ -27,24 +27,31 @@ const initialCards = [
 
 const profileEditButton = document.querySelector('.profile__edit-btn');
 const profileEditCloseButton = document.querySelector('.popup__close-btn_profile-edit');
-let profilePopup = document.querySelector('.popup_profile-edit');
+const profilePopup = document.querySelector('.popup_profile-edit');
 
-let profileEditForm = document.querySelector('.form_profile-edit');
+const profileEditForm = document.querySelector('.form_profile-edit');
 let inputName = profileEditForm.querySelector('.form__text_type_title');
 let inputCaption = profileEditForm.querySelector('.form__text_type_caption');
 
 let profileName = document.querySelector('.profile__name');
 let profileCaption = document.querySelector('.profile__caption');
 
-let cards = document.querySelector('.cards');
+const cardsContainer = document.querySelector('.cards');
 
 const cardsAddButton = document.querySelector('.profile__add-btn');
 const cardsAddCloseButton = document.querySelector('.popup__close-btn_cards-add');
-let cardAddPopup = document.querySelector('.popup_card-add');
+const cardAddPopup = document.querySelector('.popup_card-add');
 
-let cardAddForm = document.querySelector('.form_card-add');
+const cardAddForm = document.querySelector('.form_card-add');
 let cardName = cardAddForm.querySelector('.form__text_type_card-title');
 let cardLink = cardAddForm.querySelector('.form__text_type_card-link');
+
+const cardImgPopup = document.querySelector('.popup_image');
+const previewCloseButton = document.querySelector('.popup__close-btn_image-view');
+let previewImg = document.querySelector('.image-view__pic');
+let previewTitle = document.querySelector('.image-view__title');
+
+const template = document.querySelector('.card-template').content;
 
 function popupOpen(item) {
   item.classList.add ('popup_opened');
@@ -54,18 +61,13 @@ function popupClose(item) {
   item.classList.remove ('popup_opened');
 }
 
-function renderCard(item) {
-  let cardImgPopup = document.querySelector('.popup_image');
-  const previewCloseButton = document.querySelector('.popup__close-btn_image-view');
-  let previewImg = document.querySelector('.image-view__pic');
-  let previewTitle = document.querySelector('.image-view__title');
-
-  const template = document.querySelector('.card-template').content;
+function createCard(item) {
   const newCard = template.querySelector('.card').cloneNode(true);
+  let cardPopupImg = newCard.querySelector('.card__image');
 
   newCard.querySelector('.card__title').textContent = item.name;
-  newCard.querySelector('.card__image').src = item.link;
-  newCard.querySelector('.card__image').alt = item.name;
+  cardPopupImg.src = item.link;
+  cardPopupImg.alt = item.name;
 
   newCard.querySelector('.card__reaction').addEventListener('click', function(evt) {
     evt.target.classList.toggle('card__reaction_active');
@@ -75,11 +77,7 @@ function renderCard(item) {
     newCard.remove();
   });
 
-  newCard.querySelector('.card__image').addEventListener('click', function(evt) {
-    previewCloseButton.addEventListener('click', function() {
-      popupClose(cardImgPopup);
-    });
-
+  cardPopupImg.addEventListener('click', function(evt) {
     previewImg.src = item.link;
     previewImg.alt = item.name;
     previewTitle.textContent = item.name;
@@ -87,7 +85,11 @@ function renderCard(item) {
     popupOpen(cardImgPopup);
   });
 
-  cards.prepend(newCard);
+  return newCard;
+}
+
+function renderCard(item) {
+  cardsContainer.prepend(createCard(item));
 }
 
 profileEditButton.addEventListener('click', function() {
@@ -115,11 +117,21 @@ cardAddForm.addEventListener('submit', function(evt) {
   evt.preventDefault();
   const item = { name: cardName.value, link: cardLink.value };
   renderCard(item);
+
   popupClose(cardAddPopup);
+
+  cardName.value = '';
+  cardLink.value = '';
 });
 
 cardsAddCloseButton.addEventListener('click', function() {
   popupClose(cardAddPopup);
+});
+
+//beb
+
+previewCloseButton.addEventListener('click', function() {
+  popupClose(cardImgPopup);
 });
 
 initialCards.forEach(renderCard);
