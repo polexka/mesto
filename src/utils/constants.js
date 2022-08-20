3e4e6a3e0f0c0a3788beb24996cea51c518d3b90
Polina 2nd acc <lolaschmidt@yandex.ru>
2022-08-21 01:01:59 +0300

    webpack

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,16 +9,11 @@ const config = {
 
 const profileEditButton = document.querySelector('.profile__edit');
 const profilePopupSelector = '.popup_profile-edit';
-
 const profileNameSelector = '.profile__name';
 const profileCaptionSelector = '.profile__caption';
-
 const cardsContainerSelector = '.cards';
-
 const cardsAddButton = document.querySelector('.profile__add');
 const cardUploadSelector = '.popup_card-add';
-
-
 const imagePopupSelector = '.popup_image';
 
 export {
