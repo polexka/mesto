c9c4f814beaad52ef9899020466ab27804eab9b1
polechka <pol1na-tsukanova@yandex.ru>
2022-09-09 02:28:30 +0300

    proj 9: fix css confirm button

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -3,7 +3,7 @@ import Popup from './Popup.js';
 export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector, confirmCallback) {
     super(popupSelector);
-    this._button = this._popupElement.querySelector('.confirm');
+    this._button = this._popupElement.querySelector('.form__submit');
     this._confirmCallback = confirmCallback;
     this._confirm = this._confirm.bind(this);
   }
@@ -15,7 +15,6 @@ export default class PopupWithConfirmation extends Popup {
 
   open(element, id) {
     super.open();
-    console.log('opened popup');
     this._button.addEventListener('click', () => {
       this._confirm(element, id);
     });
